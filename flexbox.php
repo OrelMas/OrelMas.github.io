56e7475739010cef663c706f6d5bb4d90d501117
OrelMas <144032887+OrelMas@users.noreply.github.com>
2023-09-11 21:09:47 +0200

    Update flexbox.php

--- flexbox.php
+++ flexbox.php
@@ -1,21 +1,27 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-
-    <meta charset="UTF-8">
-    
-    <title>Cours FlexBox</title>
-    <link rel="stylesheet" href="./style.css">
-    
+    <!DOCTYPE html>
+    <html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>Cours FlexBox</title>
+        <link rel="stylesheet" href="./style.css">
 </head>
 <body>
-
     <div class="conteneur">
-    <div class="rouge">
-    <div class="vert">
-    <div class="orange"></div>
-
-
+        <div class="rouge"></div>
+        <div class="vert"></div>
+        <div class="orange"></div>
+        <div class="vert"></div>
+        <div class="rouge"></div>
+        <div class="orange"></div>    
+        <div class="vert"></div>
+        <div class="rouge"></div>
+        <div class="orange"></div>  
+        <div class="vert"></div>
+        <div class="rouge"></div>
+        <div class="orange"></div>  
+    </div>
+</body>
+</html>
 
 
 
